5644920eb9f91abc9e2f21f09ffe30001b7e2000
Diegocarrero <diegocar2413@gmail.com>
2023-03-26 09:44:50 -0400

    Add cypress and heroku fixes, create Nav, add route to home

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { Route, Routes } from "react-router-dom";
+
 function App() {
   return (
     <div className="app">
       <h1>Welcome to GhibliApp</h1>
+
+      <Routes>
+          <Route path="/" />
+          <Route path="/shows" element={<ShowsIndex />} />
+          <Route path="/shows/new" element={<ShowsNewForm />} />
+          <Route path="/shows/:id" element={<Show />} />
+          <Route path="/shows/:id/edit" element={<ShowsEditForm />} />
+          <Route path="/movies" element={<MoviesIndex />} />
+          <Route path="/movies/new" element={<MoviesNewForm />} />
+          <Route path="/movies/:id" element={<Movie />} />
+          <Route path="/movies/:id/edit" element={<MoviesEditForm />} />
+      </Routes>
     </div>
   );
 }
