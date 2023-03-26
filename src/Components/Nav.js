065d2ff5925639a91b83561cab4bcfc6ae533264
laura-williams-1 <laurawilliams@pursuit.org>
2023-03-26 09:47:17 -0400

    Nav Bar is functional

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,3 +1,24 @@
+import { NavLink } from "react-router-dom";
+
 export default function Nav() {
-  return <nav></nav>;
+  return (
+    <nav>
+      <div>
+        <ul>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/movies">Movies</NavLink>
+          </li>
+          <li>
+            <NavLink to="/people">People</NavLink>
+          </li>
+          <li>
+            <NavLink to="/locations">Locations</NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
