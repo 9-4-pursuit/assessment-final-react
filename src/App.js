8aafaf410c9b9f02882883ee78f725939ad42833
JohnnyMCR <jmcastillo991@gmail.com>
2023-03-26 13:21:28 -0400

    So many edits

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import { BrowserRouter } from "react-router-dom";
-import { Routes, Route } from "react-router-dom";
-
-
+import { BrowserRouter, Router, Routes, Route } from "react-router-dom";
+// import { Link } from "react-router-dom";
+import Home from "./Components/Home";
+import Films from "./Components/Films";
+import Locations from "./Components/Locations";
+import People from "./Components/People";
+import NavBar from "./Components/NavBar";
 
 
 function App() {
   return (
-    <div className="app">
-      <h1>Welcome to GhibliApp</h1>
+    <div className="container">
+    <Router>
+      <div className="header">
+       <NavBar />
+       </div>
+        <div className="main">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/films" element={<Films />} />
+          <Route path="/people" element={<People />} />
+          <Route path="/locations" element={<Locations />} />
+        </Routes>
+      </div>
+    </Router>
     </div>
   );
 }
--- src/Components/Films.js
+++ src/Components/Films.js
@@ -1,2 +1,51 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { getAllMovies } from "../Fetch";
 
+const Films = () => {
+    const [movies, setFilms] = useState([]);
+    const [result, setResult] = useState([]);
+  
+    useEffect(() => {
+      getAllMovies().then((films) => {
+        setFilms(films);
+      });
+    }, []);
+  
+    const handleSelect = (e) => {
+      const { value } = e.target;
+      const selected = movies.find(({ id }) => id === value);
+      if (!selected) {
+        setResult("");
+      } else {
+        const { title, release_date, description } = selected;
+        setResult(
+          <div className='item'>
+            <h1> Title: {title}</h1>
+            <p>Release Date: {release_date}</p>
+            <p>Description: {description}</p>
+          </div>
+        );
+      }
+    };
+  
+    return (
+      <div className='movies'>
+        <h1>Select A Movie</h1>
+        <div>
+          <select onChange={handleSelect}>
+            <option value=''></option>
+            {movies.map((movie) => (
+              <option key={movie.id} value={movie.id}>
+                {movie.title}
+              </option>
+            ))}
+          </select>
+          <br />
+          {result}
+        </div>
+      </div>
+    );
+  };
 
+export default Films;
