19dd6fd1669c5ba87b81cf9176934be31422ad4d
Adrian Burke <114108495+AdrianBurke1@users.noreply.github.com>
2023-03-26 10:42:32 -0400

    assessment-final-react

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,13 +1,28 @@
 import React from "react"
+import { Link } from "react-router-dom";
 
 function Navbar () {
 
-    return(
-
-        <div className="navbar">
-            
-        </div>
-    )
-}
-
+    return (
+        <nav>
+          <div className='navbar'>
+            <ul className='nav-links'>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+              <li>
+                <Link to='/movies'>Movies</Link>
+              </li>
+              <li>
+                <Link to='/people'>People</Link>
+              </li>
+              <li>
+                <Link to='/locations'>Locations</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      );
+    };
+    
 export default Navbar;
