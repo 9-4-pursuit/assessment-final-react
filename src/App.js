1d9d0e5410e8ac106c5dd746c67ef5f4bc9d03fd
Adrian Burke <114108495+AdrianBurke1@users.noreply.github.com>
2023-03-26 10:25:57 -0400

    assessment-final-react

--- src/App.js
+++ src/App.js
@@ -1,3 +1,10 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Components/Home";
+import Nav from "./Components/Navbar";
+import Movies from "./Components/Movies";
+import Locations from "./Components/Locations";
+import People from "./Components/People";
+
 function App() {
   return (
     <div className="app">
