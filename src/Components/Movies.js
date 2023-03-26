94e8f7633402962c83360f912ae002ce8bd3f43a
laura-williams-1 <laurawilliams@pursuit.org>
2023-03-26 09:51:00 -0400

    select drop down created for movies without api call

--- src/Components/Movies.js
+++ src/Components/Movies.js
@@ -1,3 +1,10 @@
 export default function Movies() {
-  return <div className="movies"></div>;
+  return (
+    <div className="movies">
+      <h1>Select a Movie</h1>
+      <select>
+        <option value="">Please select an option</option>
+      </select>
+    </div>
+  );
 }
